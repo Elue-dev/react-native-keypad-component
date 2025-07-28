e0b43d04607f6f065101fda6bc53f60043682d39
Wisdom Elue <eluewisdom@gmail.com>
2025-07-28 17:28:31 +0100

    chore: update lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,48 @@
 module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
   rules: {
     'react-native/no-inline-styles': 'off',
+    'react/react-in-jsx-scope': 'off',
     'react/no-unstable-nested-components': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'react-native/no-color-literals': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'react-native/sort-styles': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+  ignorePatterns: ['node_modules/', 'dist/', 'build/'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
